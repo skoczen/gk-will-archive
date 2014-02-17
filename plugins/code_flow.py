8f06c64cc9868395c885be2b707a70b6488f5e24
skoczen <skoczen@gmail.com>
2014-02-17 12:21:49 -0800

    Updates CR to point to scrapbin

--- plugins/code_flow.py
+++ plugins/code_flow.py
@@ -6,16 +6,16 @@ class CodeFlowPlugin(WillPlugin):
 
     @respond_to("CR (?:for )?(?P<pr_id>\d*)$")
     def cr(self, message, pr_id):
-        self.say("@all (crrequest) https://github.com/greenkahuna/gk/pull/%s" % pr_id, message=message)
+        self.say("@all (crrequest) https://github.com/greenkahuna/scrapbin/pull/%s" % pr_id, message=message)
 
     @respond_to("CR (?:for )?(?P<pr_id>\d*) g2g$")
     def cr_g2g(self, message, pr_id):
         self.say("Thanks! (crpass) for %s" % pr_id, message=message)
-        self.say("@all (frrequest) https://github.com/greenkahuna/gk/pull/%s" % pr_id, message=message)
+        self.say("@all (frrequest) https://github.com/greenkahuna/scrapbin/pull/%s" % pr_id, message=message)
 
     @respond_to("FR (?:for )?(?P<pr_id>\d*)$")
     def fr(self, message, pr_id):
-        self.say("@all (frrequest) https://github.com/greenkahuna/gk/pull/%s" % pr_id, message=message)
+        self.say("@all (frrequest) https://github.com/greenkahuna/scrapbin/pull/%s" % pr_id, message=message)
 
     @respond_to("FR (?:for )?(?P<pr_id>\d*) g2g$")
     def fr_g2g(self, message, pr_id):
