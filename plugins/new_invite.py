3952bdb652d25907ba429ebfc6eab2e730a6d090
skoczen <skoczen@gmail.com>
2014-03-03 16:15:02 -0800

    New invite plugin working.

--- plugins/new_invite.py
+++ plugins/new_invite.py
@@ -4,7 +4,8 @@ from will.decorators import respond_to, periodic, hear, randomly, route, rendere
 
 class NewInvitePlugin(WillPlugin):
 
-    @route("/new-invite/?")
+    @route("/new-invite/")
     def new_invite_listener(self):
-        new_invite_html = rendered_template("new_invite.html", request.forms)
+        new_invite_html = rendered_template("new_invite.html", self.request.query)
         self.say(new_invite_html, html=True)
+        return ""
