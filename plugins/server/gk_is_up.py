5891159d3b6c6a1dd02b47dd3450dfac55974956
Eric Carmichael <eric@ckcollab.com>
2014-03-04 16:30:35 -0800

    added 'the' to on fire list commands

--- plugins/server/gk_is_up.py
+++ plugins/server/gk_is_up.py
@@ -34,13 +34,13 @@ class UptimePlugin(WillPlugin):
     def sb_is_up(self):
         self._verify_url("https://www.scrapbin.com")
 
-    @respond_to("^add (?P<email>.*) to on fire list", multiline=True)
+    @respond_to("^add (?P<email>.*) to the on fire list", multiline=True)
     def add_to_fire_list(self, message, email=""):
         on_fire_list = self.load("on_fire_list", [])
         on_fire_list.append(email)
 
         self.save("on_fire_list", on_fire_list)
-        self.say("Got it, added %s to on fire list" % email, message=message)
+        self.say("Got it, added %s to the on fire list" % email, message=message)
 
     @respond_to("^on fire list", multiline=True)
     def get_fire_list(self, message):
@@ -48,15 +48,15 @@ class UptimePlugin(WillPlugin):
         fire_list_html = rendered_template("fire_list.html", {"fire_list": on_fire_list})
         self.say(fire_list_html, message=message, html=True)
 
-    @respond_to("^remove (?P<email>.*) from on fire list", multiline=True)
+    @respond_to("^remove (?P<email>.*) from the on fire list", multiline=True)
     def remove_from_fire_list(self, message, email=""):
         on_fire_list = self.load("on_fire_list", [])
         on_fire_list.remove(email)
 
         self.save("on_fire_list", on_fire_list)
-        self.say("Got it, removed %s from on fire list" % email, message=message)
+        self.say("Got it, removed %s from the on fire list" % email, message=message)
 
-    @respond_to("^send test email to on fire list", multiline=True)
+    @respond_to("^send test email to the on fire list", multiline=True)
     def test_on_fire_emails(self, message):
         on_fire_list = self.load("on_fire_list", [])
 
