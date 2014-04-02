84ab35ed559f4236ac8ef570317d79cb86e12e7d
skoczen <skoczen@gmail.com>
2014-04-02 09:14:16 -0700

    Adds some debug for figuring out a clever staging solve.

--- plugins/new_invite.py
+++ plugins/new_invite.py
@@ -7,7 +7,7 @@ class NewInvitePlugin(WillPlugin):
     @route("/new-invite/")
     def new_invite_listener(self):
         from plugins.zoho_crm import ZohoCRMPlugin as ZohoClient
-
+        print self.request.headers.__dict__
         new_invite_html = rendered_template("new_invite.html", self.request.query)
         self.say(new_invite_html, html=True)
         
