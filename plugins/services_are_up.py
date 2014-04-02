78c40561d7f3c1c0e8b7765b6095a03879701a65
skoczen <skoczen@gmail.com>
2014-04-01 17:07:53 -0700

    Teaches will how to let us know when heroku's down as well.

--- plugins/services_are_up.py
+++ plugins/services_are_up.py
@@ -16,3 +16,14 @@ class ServicesPlugin(WillPlugin):
             else:
                 self.say("Looks like github's back up!")
             self.save("last_github_status", r.json()["status"])
+
+    @periodic(second='46')
+    def heroku_is_up(self):
+        r = requests.get("https://status.heroku.com/api/v3/current-status")
+        last_status = self.load("last_heroku_status")
+        if r.json()["status"] != last_status:
+            if r.json()["status"]["Production"] != "green":
+                self.say("FYI everyone, heroku is having trouble: %s. \n http://status.heroku.com" % r.json()["issues"][0]["title"])
+            else:
+                self.say("Looks like heroku's back up!")
+            self.save("last_heroku_status", r.json()["status"])
