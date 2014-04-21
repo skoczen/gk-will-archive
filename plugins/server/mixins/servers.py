dc81ef93924b0ffc97b22daf24ccb73dc79dd78c
skoczen <skoczen@gmail.com>
2014-04-21 12:01:05 -0700

    geeeting sooo haaacckkyyyyyyy..

--- plugins/server/mixins/servers.py
+++ plugins/server/mixins/servers.py
@@ -425,10 +425,9 @@ StrictHostKeyChecking no
                             if "already installed" in e:
                                 self.run_heroku_cli_command("addons:remove %s --confirm %s" % (addon, self.stack.url_name))
                                 self.run_heroku_cli_command("addons:add %s --confirm %s" % (addon, self.stack.url_name))
-                                self.addons.append(addon)
 
                 for addon_name in self.addons:
-                    if addon_name not in self.stack.deploy_config["heroku"]["addons"]:
+                    if addon_name.split(" ")[0] not in self.stack.deploy_config["heroku"]["addons"]:
                         self.run_heroku_cli_command("addons:remove %s --confirm %s" % (addon_name, self.stack.url_name))
                         self.add_to_saved_output(" - %s (removed)" % addon)
 
