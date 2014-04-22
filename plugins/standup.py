7b8feae3d577f571d65df97e343f9bc3f0841477
skoczen <skoczen@gmail.com>
2014-04-22 11:30:23 -0700

    Teaches will 8:30 standup

--- plugins/standup.py
+++ plugins/standup.py
@@ -4,6 +4,6 @@ from will import settings
 
 class StandupPlugin(WillPlugin):
 
-    @periodic(hour='11', minute='30', day_of_week="mon-fri")
+    @periodic(hour='8', minute='30', day_of_week="mon-fri")
     def standup(self):
         self.say("@all Standup! %s" % settings.WILL_ZOOM_URL)
